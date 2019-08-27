0f06e7f5eee1ee8e83ea2145d0514e6ca4c56d8e
Santiago Moritan <smoritan@icloud.com>
2019-08-26 21:41:38 -0300

    ready test reservaHorario

--- test/test.js
+++ test/test.js
@@ -1,6 +1,32 @@
+
 var expect = chai.expect;
-describe("Testeo function reservaHorario", function(){
+
+describe("Testeo función reservaHorario()", function(){
+    var restoPrueba;
+    beforeEach(function(){
+        restoPrueba  = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+    });
+
     it("Probando que el horario reservado se elimine del array", function(){
-        expect(reservarHorario(11.00)).to.be.true;
+        restoPrueba.reservarHorario("13:00")
+        expect(restoPrueba.horarios).not.to.include("13:00")
     })
+    it("Probando que horario no disponible no cambie el array", function(){
+        var arrayOrigin =  [...restoPrueba.horarios];
+        restoPrueba.reservarHorario("21:00")
+        expect(restoPrueba.horarios).to.eql(arrayOrigin);
+    })
+    it("Probando que si la función se ejecura sin el horario en los parametro, el array no se modifique", function(){
+        var arrayOrigin = [...restoPrueba.horarios];
+        restoPrueba.reservarHorario()
+        expect(restoPrueba.horarios).to.eql(arrayOrigin);
+    })
+});
+describe("Testeo función obtenerPuntuación()", function(){
+    var restoPrueba;
+    beforeEach(function(){
+        restoPrueba  = new Restaurant(1, "TAO Uptown", "Asiática", "Nueva York", ["13:00", "15:30", "18:00"], "../img/asiatica1.jpg", [6, 7, 9, 10, 5]);
+    });
+
+    it("")
 })
